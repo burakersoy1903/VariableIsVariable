d9a37b4f39a167e6c532d2b4f78df2f398bf08e1
burakersoy1903 <burakersoy1903@gmail.com>
2022-09-29 07:50:32 -0500

    Challenge 1 solved!

--- Set1Challenge1.py
+++ Set1Challenge1.py
@@ -10,14 +10,47 @@
 # Well let's learn before we implement
 # https://youtu.be/Yg1ZWegeZiM
 
+#https://docs.python.org/3/library/base64.html
+
+#Research RFC 4648
+
 #See below for some fun exercises & documentation
 ########################################################
 
+#You'll need to use this code for the rest of the exercises. Encapsulate it, baby, into a function that we could reuse
 from base64 import b16decode, b64encode 
-data_hex = b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-data_raw = b16decode(data_hex, casefold=True)
-data_b64 = b64encode(data_raw)
-print(data_b64)
+def hex_to_b64(data_hex: bytes) -> bytes: #Type signatures to remind that we're working with bytes and not strings
+    return b64encode(b16decode(data_hex, casefold=True))
+
+#If we were to import the script, it will run all of this including the logic and print statements
+#Logic incur unnecessary compute and print statements would pollute standard out
+#It would be very confusing to whoever's importing this
+#Imported and run in main vs. running the Set1Challenge1.py seperately
+if __name__ == "__main__":
+    data_hex = b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    data_b64 = hex_to_b64(data_hex)
+
+    print(f"{data_hex = }")
+    print(f"{data_b64 = }")
+    
+    #Let's check if our result is correct from challenge 1
+    if data_b64 == b"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t":
+        print("It worked!")
+    else:
+        exit("Conversion failed (this should never happen!")
+
+
+########################################################
+###Conversion Done
+#from base64 import b16decode, b64encode 
+#data_hex = b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d" #byte literal
+#data_raw = b16decode(data_hex, casefold=True) #Solved the case sensitive nature of b16decode
+#data_b64 = b64encode(data_raw)
+#print(f"{data_hex = }") #pretty printing
+#print(f"{data_raw = }")
+#print(f"{data_b64 = }")
+########################################################
+
 
 ########################################################
 ###Exploring b16decode
@@ -29,20 +62,6 @@ print(data_b64)
 ##help(b16decode) #Optional casefold is a flag specifying whether a lowercase alphabet is acceptable as input.  For security purposes, the default is False.
 
 #print(b16decode(data_hex, casefold=True)) #other solution #ignores the case sensitivity
-########################################################
-
-
-########################################################
-###Exploring encoding to ASCII directly #In progress #Continue testing encoding and decoding
-#import base64
-
-#message = b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-##message_bytes = message.encode('ascii')
-#base64_bytes = base64.b64encode(message)
-#print(base64_bytes)
-#base64_message = base64_bytes.decode('ascii')
-
-#print(base64_message)
 ########################################################
 
 
