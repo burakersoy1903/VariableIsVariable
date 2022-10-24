309bcafea16f21c17195e730fd641f850cacf513
burakersoy1903 <burakersoy1903@gmail.com>
2022-10-24 08:07:47 -0500

    Python Ordinals

--- Set1Challenge2.py
+++ Set1Challenge2.py
@@ -20,28 +20,38 @@ b = bytes.fromhex("686974207468652062756c6c277320657965")
 
 
 ########################################################
+###Ordinals
+###https://medium.com/codex/python-ord-getting-the-ordinal-value-of-a-unicode-character-57513b061105#:~:text=The%20term%20ordinal%20means%20countable,encoded%20representations%2C%20or%20sheer%20hobbyism.
+###The term ordinal means countable and in the context of Python's ord function will return the integer value assigned to a character in the Unicode encoding system. 
+###This can be useful when converting between bytes to strings, binary to encoded representations, or sheer hobbyism.
+for ch in b'abc':
+    print(ch)
+print(ord('a'))
+########################################################
+
+########################################################
 ###Let's benchmark different methods
-import timeit
+#import timeit
 #code snippet to be executed only once
-mysetup = ""
+#mysetup = ""
 #code snippet whose execution time is to be measured
-mycode = '''
-def iter_1(a,b):
-    for i in range(min(len(a), len(b))):
-        a[i], b[i]
-'''
+#mycode = '''
+#def iter_1(a,b):
+#    for i in range(min(len(a), len(b))):
+#        a[i], b[i]
+#'''
 #timeit statement
-print (timeit.timeit(setup = mysetup,
-                     stmt = mycode,
-                     number = 10000)) #the execution time(in seconds) for 10000 iterations of the code snippet
+#print (timeit.timeit(setup = mysetup,
+#                     stmt = mycode,
+#                     number = 10000)) #the execution time(in seconds) for 10000 iterations of the code snippet
 #code snippet whose execution time is to be measured
-mycode = '''
-def iter_2(a,b):
-    for byte_1, byte_2 in zip(a,b):
-        byte_1, byte_2
-'''
+#mycode = '''
+#def iter_2(a,b):
+#    for byte_1, byte_2 in zip(a,b):
+#        byte_1, byte_2
+#'''
 #timeit statement
-print (timeit.timeit(setup = mysetup,
-                     stmt = mycode,
-                     number = 10000)) #the execution time(in seconds) for 10000 iterations of the code snippet
+#print (timeit.timeit(setup = mysetup,
+#                     stmt = mycode,
+#                     number = 10000)) #the execution time(in seconds) for 10000 iterations of the code snippet
 ########################################################
